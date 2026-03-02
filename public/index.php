21e7eba76326e7eeb22d9644c592cc7735e14adf
Tran Duc Thang <emailgithubcuaban@gmail.com>
2026-03-02 13:51:59 +0700

    d

--- public/index.php
+++ public/index.php
@@ -5,12 +5,30 @@ use Illuminate\Http\Request;
 
 define('LARAVEL_START', microtime(true));
 
+/*
+|--------------------------------------------------------------------------
+| Check If The Application Is Under Maintenance
+|--------------------------------------------------------------------------
+*/
+
 if (file_exists($maintenance = __DIR__.'/tdmappro/storage/framework/maintenance.php')) {
     require $maintenance;
 }
 
+/*
+|--------------------------------------------------------------------------
+| Register The Auto Loader
+|--------------------------------------------------------------------------
+*/
+
 require __DIR__.'/tdmappro/vendor/autoload.php';
+
+/*
+|--------------------------------------------------------------------------
+| Run The Application
+|--------------------------------------------------------------------------
+*/
 
 $app = require_once __DIR__.'/tdmappro/bootstrap/app.php';
 
-$app->handleRequest(Request::capture());
+$app->handleRequest(Request::capture());d
