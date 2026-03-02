f009b5cf612573f0432fabc1ddb343b1968c18f2
Tran Duc Thang <emailgithubcuaban@gmail.com>
2026-03-02 14:00:58 +0700

    d

--- public/index.php
+++ public/index.php
@@ -5,30 +5,16 @@ use Illuminate\Http\Request;
 
 define('LARAVEL_START', microtime(true));
 
-/*
-|--------------------------------------------------------------------------
-| Check If The Application Is Under Maintenance
-|--------------------------------------------------------------------------
-*/
-
-if (file_exists($maintenance = __DIR__.'/tdmappro/storage/framework/maintenance.php')) {
+// Determine if the application is in maintenance mode...
+if (file_exists($maintenance = __DIR__.'/../storage/framework/maintenance.php')) {
     require $maintenance;
 }
 
-/*
-|--------------------------------------------------------------------------
-| Register The Auto Loader
-|--------------------------------------------------------------------------
-*/
+// Register the Composer autoloader...
+require __DIR__.'/../vendor/autoload.php';
 
-require __DIR__.'/tdmappro/vendor/autoload.php';
-
-/*
-|--------------------------------------------------------------------------
-| Run The Application
-|--------------------------------------------------------------------------
-*/
-
-$app = require_once __DIR__.'/tdmappro/bootstrap/app.php';
+// Bootstrap Laravel and handle the request...
+/** @var Application $app */
+$app = require_once __DIR__.'/../bootstrap/app.php';
 
 $app->handleRequest(Request::capture());
